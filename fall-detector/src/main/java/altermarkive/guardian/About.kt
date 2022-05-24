77302c0e518889bab39716969f778ae7ff682754
YuseonChoi <chldbtjs74@naver.com>
2022-05-24 21:34:59 +0900

    add fonts

--- fall-detector/src/main/java/altermarkive/guardian/About.kt
+++ fall-detector/src/main/java/altermarkive/guardian/About.kt
@@ -26,8 +26,8 @@ class About : Fragment(), View.OnClickListener {
     ): View? {
         val binding = inflater.inflate(R.layout.about, container, false)
         this.binding = binding
-        val web = binding.findViewById<View>(R.id.information) as WebView
-        web.loadUrl("file:///android_asset/about.html")
+//        val web = binding.findViewById<View>(R.id.information) as WebView
+//        web.loadUrl("file:///android_asset/about.html")
         val emergency = binding.findViewById<View>(R.id.emergency) as Button
         emergency.setOnClickListener(this)
         return binding
--- fall-detector/src/main/java/altermarkive/guardian/Main.kt
+++ fall-detector/src/main/java/altermarkive/guardian/Main.kt
@@ -27,26 +27,26 @@ class Main : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
     lateinit var drawerLayout: DrawerLayout
 
 
-    private fun eula(context: Context) {
-        // Run the guardian
-        Guardian.initiate(this)
-        // Load the EULA
-        val dialog = Dialog(context)
-        dialog.setContentView(R.layout.eula)
-        dialog.setTitle("EULA")
-        val web = dialog.findViewById<View>(R.id.eula) as WebView
-        web.loadUrl("file:///android_asset/eula.html")
-        val accept = dialog.findViewById<View>(R.id.accept) as Button
-        accept.setOnClickListener { dialog.dismiss() }
-        val layout = WindowManager.LayoutParams()
-        val window = dialog.window
-        window ?: return
-        layout.copyFrom(window.attributes)
-        layout.width = WindowManager.LayoutParams.MATCH_PARENT
-        layout.height = WindowManager.LayoutParams.MATCH_PARENT
-        window.attributes = layout
-        dialog.show()
-    }
+//    private fun eula(context: Context) {
+//        // Run the guardian
+//        Guardian.initiate(this)
+//        // Load the EULA
+//        val dialog = Dialog(context)
+//        dialog.setContentView(R.layout.eula)
+//        dialog.setTitle("EULA")
+//        val web = dialog.findViewById<View>(R.id.eula) as WebView
+//        web.loadUrl("file:///android_asset/eula.html")
+//        val accept = dialog.findViewById<View>(R.id.accept) as Button
+//        accept.setOnClickListener { dialog.dismiss() }
+//        val layout = WindowManager.LayoutParams()
+//        val window = dialog.window
+//        window ?: return
+//        layout.copyFrom(window.attributes)
+//        layout.width = WindowManager.LayoutParams.MATCH_PARENT
+//        layout.height = WindowManager.LayoutParams.MATCH_PARENT
+//        window.attributes = layout
+//        dialog.show()
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,7 +77,7 @@ class Main : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
         navigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
 
-        eula(this)
+//        eula(this)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
